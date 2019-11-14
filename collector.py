278fd67e66d317d37d8c41cd8da2df0e41846331
francesco-pez1 <56473264+francesco-pez1@users.noreply.github.com>
2019-11-14 17:09:17 +0100

    Add files via upload

--- collector.py
+++ collector.py
@@ -4,18 +4,19 @@ import time
 import urllib.request, urllib.error, urllib.parse
 import os.path
 from collector_utils import PRINT
-
+import json
 #ConnectionError
 
 # where we want save all the html files
 save_path = '/home/lex/Desktop/Data_science/Algorithmic_Methods_of_Data_Mining/ADM_hw3/HTML'
 
+#number_link_title = {}
 
 # counter for the name of the file
-counter = 19994
+counter = 0#20530#5010
 
 # A loop for each list of link
-for i in range(3,4):
+for i in range(1,4):
     
     # We are reading the HTML with all the links for the pages of wikipedia
     list_of_pages = 'https://raw.githubusercontent.com/CriMenghini/ADM/master/2019/Homework_3/data/movies' + str(i) + '.html'               
@@ -34,54 +35,23 @@ for i in range(3,4):
             print(i,j,counter)
             # read the j-th page of wikipedia in the i-th HTML page of all links
             response = urllib.request.urlopen(wiki_links[j].get('href'))
-            webContent = response.read()
+            webContent = response.read().decode(response.headers.get_content_charset())
             
             # this function save this page in an .html file inside the folder at the address save_path 
             PRINT(save_path, counter, webContent)
-            counter += 1
+            soup_i = BeautifulSoup(webContent, 'html5lib')
+            #a = soup_i.title.text[:-12]
+            #number_link_title[counter] = (soup_i.title.text[:-12], wiki_links[j].get('href'))
+            
         #except 'error409':
         #    time.sleep(1200)
         except:
+            PRINT(save_path, counter, 'NA')
+            
             pass
+        counter += 1
         time.sleep(0.01)
 
-
-
-'''
-
-import urllib.request
- 
-url= 'https://asd.com/asdID='
-for i in range(1, 5):
-    print('     --> ID:', i)
-    newurl = url + str(i)
-    f = open(str(i)+'.html', 'w')
-    page = urllib.request.urlopen(wiki_links[i].get('href'))
-    pagetext = str(page.read())
-    f.write(pagetext)
-    f.close()
-    
-    
-    
-    
-    
-#fid=urllib.request.urlopen(link_i)
-fid = urllib.request.urlopen(wiki_links[i].get('href'))
-fi = fid.read()
-webpage=fid.read().decode('utf-8')
-
-
-
-save_path = '/home/lex/Desktop/Data_science/Algorithmic_Methods_of_Data_Mining/ADM_hw3/HTML'
-
-name_of_file = 'article_' + str(i) + '.html'
-
-completeName = os.path.join(save_path, name_of_file)
-with open(completeName, 'w') as out_file:
-    out_file.write('please')
-
-
-'''
 
 
 
